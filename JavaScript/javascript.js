d7a3bbd2884ee602da81df401788dcd77582d99d
hopembuthye38 <hope.mbuthye.moringaschool.com>
2021-07-19 14:23:10 +0300

    check validity of data

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -2,3 +2,15 @@ let daysoftheweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fr
 let malenames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let femalenames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",]
 
+function validateform() {
+    let year = document.getElementById("YearField").value;
+    let month = document.getElementById("MonthField").value;
+    let day = document.getElementById("DateField").value;
+
+    if (year == "" || month == "" || day == "") {
+        console.log()
+        alert("Please fill in all deatils")
+        return false;
+    } else {
+        getuserdetails()   }
+}
