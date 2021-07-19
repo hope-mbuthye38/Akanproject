0e2cce152dde1d1c7ef278d0e5020a430937e612
hopembuthye38 <hope.mbuthye.moringaschool.com>
2021-07-19 14:21:55 +0300

    add daysOfThe week,male&female names

---
+++ JavaScript/javascript.js
@@ -0,0 +1,4 @@
+let daysoftheweek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let malenames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let femalenames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",]
+
